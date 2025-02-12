31d4b4b49908b494b8a37e5aaddc7dd21dabaebd
Jordi <jadoumie@berkeley.edu>
2025-02-12 09:33:20 -0500

    more functionality in

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,8 +1,14 @@
-import { Image, Text, View } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { Link } from "expo-router";
-import { SafeAreaView } from "react-native";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import Search from "@/components/search";
 
 export default function Index() {
   return (
@@ -21,6 +27,19 @@ export default function Index() {
             </View>
           </View>
           <Image source={icons.bell} className="size-6" />
+        </View>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </SafeAreaView>
