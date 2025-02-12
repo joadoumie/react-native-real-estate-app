f416f340c85e16c3dc4ef5d860baec2adf34c442
Jordi <jadoumie@berkeley.edu>
2025-02-12 14:06:21 -0500

    home screen minus scrolling completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,6 +9,8 @@ import { Link } from "expo-router";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/search";
+import { Card, FeaturedCard } from "@/components/cards";
+import Filters from "@/components/filters";
 
 export default function Index() {
   return (
@@ -40,6 +42,26 @@ export default function Index() {
               </Text>
             </TouchableOpacity>
           </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendations
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Filters />
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
+          <Card />
         </View>
       </View>
     </SafeAreaView>
